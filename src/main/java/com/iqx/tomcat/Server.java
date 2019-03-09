23c67b345d4713c479590e18693b58a6c29996d8
ZehuaWang <transforminottawa@gmail.com>
2019-03-09 11:57:31 -0500

    Add two method: 1. parse the client input stream and get the resource name. 2. Send the static resource back to client.

--- src/main/java/com/iqx/tomcat/Server.java
+++ src/main/java/com/iqx/tomcat/Server.java
@@ -1,4 +1,67 @@
 package com.iqx.tomcat;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    //Define a variable to store the absolute path of web content.
+    public static String WEB_ROOT = System.getProperty("user.dir")+"\\"+"resources";
+
+    private String url = "";
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        Socket socket  = null;
+        InputStream is = null;
+        OutputStream ops = null;
+
+        try {
+            serverSocket = new ServerSocket(8080);
+
+            while (true) {
+                socket = serverSocket.accept();
+                is  = socket.getInputStream();
+                ops = socket.getOutputStream();
+                parse(is);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(null != is) {
+                is.close();
+                is = null;
+            }
+
+            if(null != ops) {
+                ops.close();
+                ops = null;
+            }
+
+            if(null != socket) {
+                socket.close();
+                socket = null;
+            }
+        }
+    }
+
+    /**
+     * Get the resource name from input stream
+     */
+    private static void parse(InputStream is) {
+
+
+    }
+
+    /**
+     * Send the static resource
+     */
+    private static void sendStaticResource(OutputStream ops) {
+
+    }
 }
