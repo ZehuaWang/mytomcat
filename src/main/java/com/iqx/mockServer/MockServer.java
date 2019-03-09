ede5a5de713f480af25bdce3d01a52836d82c6d6
ZehuaWang <transforminottawa@gmail.com>
2019-03-09 11:22:42 -0500

    Add a simple application that used to send respond to the client request.

--- src/main/java/com/iqx/mockServer/MockServer.java
+++ src/main/java/com/iqx/mockServer/MockServer.java
@@ -1,6 +1,62 @@
 package com.iqx.mockServer;
 
-public class MockServer {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+/**
+ * A simple server side application to respond the client request
+ * @author Eric
+ */
+public class MockServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        OutputStream ops = null;
+        try{
+            serverSocket = new ServerSocket(8080);
+
+            while (true) {
+                socket = serverSocket.accept();
+
+                ops = socket.getOutputStream();
+
+                ops.write("HTTP/1.1 200 OK\n".getBytes());
+
+                ops.write("Content-Type:text/html;charset=urf-8\n".getBytes());
+
+                ops.write("Server:Apache-Coyote/1.1\n".getBytes());
+
+                ops.write("\n\n".getBytes());
+
+                StringBuffer sb = new StringBuffer();
+
+                sb.append("<html>");
+
+                sb.append("<head><title>Title</title></head>");
+
+                sb.append("</html>");
+
+                ops.write(sb.toString().getBytes());
+
+                ops.flush();
+            }
+
+        } catch (Exception e) {
+
+
+
+        } finally {
+
+            if(null != ops)
+                ops.close();
+
+            if(null != socket)
+                socket.close();
+
+        }
+
+    }
 
 }
