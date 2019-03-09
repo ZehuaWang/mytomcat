4cf10aaa587d5d8be3056e833f8a5389b3c8ad86
ZehuaWang <transforminottawa@gmail.com>
2019-03-09 16:11:44 -0500

    Implement the method to send static resource and parse client request url.

--- src/main/java/com/iqx/tomcat/Server.java
+++ src/main/java/com/iqx/tomcat/Server.java
@@ -1,8 +1,6 @@
 package com.iqx.tomcat;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,9 +9,9 @@ public class Server {
     //Define a variable to store the absolute path of web content.
     public static String WEB_ROOT = System.getProperty("user.dir")+"\\"+"resources";
 
-    private String url = "";
+    private static String url = "";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = null;
         Socket socket  = null;
         InputStream is = null;
@@ -27,7 +25,7 @@ public class Server {
                 is  = socket.getInputStream();
                 ops = socket.getOutputStream();
                 parse(is);
-
+                sendStaticResource(ops);
             }
 
         } catch (IOException e) {
@@ -53,15 +51,87 @@ public class Server {
     /**
      * Get the resource name from input stream
      */
-    private static void parse(InputStream is) {
+    private static void parse(InputStream is) throws Exception {
 
+        StringBuffer content = new StringBuffer(2048);
+        byte[] buffer = new byte[2048];
+        int i = -1;
+        i=is.read(buffer);
+        for(int j=0; j<i; j++) {
+            content.append((char)buffer[j]);
+        }
 
+        System.out.println(content);
+
+        parseUrl(content.toString());
     }
 
     /**
      * Send the static resource
      */
-    private static void sendStaticResource(OutputStream ops) {
+    private static void sendStaticResource(OutputStream ops) throws IOException {
 
+        byte[] bytes = new byte[2048];
+        FileInputStream fis = null;
+
+        try {
+
+            File file = new File(WEB_ROOT,url);
+
+            System.out.print(WEB_ROOT);
+            System.out.print(url);
+
+            if(file.exists()) {
+
+                ops.write("HTTP/1.1 200 OK\n".getBytes());
+                ops.write("Server:apache-Coyote/1.1\n".getBytes());
+                ops.write("Content-Type:text/html;charset=utf-8\n".getBytes());
+                ops.write("\n".getBytes());
+
+                fis = new FileInputStream(file);
+
+                int ch = fis.read(bytes);
+
+                while (ch!=-1) {
+                    ops.write(bytes,0,ch);
+                }
+
+            } else {
+
+                ops.write("HTTP/1.1 404 not found\n".getBytes());
+                ops.write("Server:apache-Coyote/1.1\n".getBytes());
+                ops.write("Content-Type:text/html;charset=utf-8\n".getBytes());
+                ops.write("\n".getBytes());
+
+                String errorMessage = "file not found";
+                ops.write(errorMessage.getBytes());
+            }
+
+        } catch (Exception e) {
+                e.printStackTrace();
+        } finally {
+            if(null!=fis) {
+                fis.close();
+                fis = null;
+            }
+        }
+
+    }
+
+    /**
+     * Get the resource name
+     */
+    private static void parseUrl(String content) {
+        int index1,index2;
+        index1 = content.indexOf(" ");
+        if(index1 != -1) {
+            index2 = content.indexOf(" ",index1+1);
+            if(index2 > index1) {
+
+                url = content.substring(index1+2, index2);
+            }
+        }
+
+        System.out.println(url);
     }
 }
